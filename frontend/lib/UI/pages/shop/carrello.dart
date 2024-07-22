5d7c49df52bcdb17d346804ecc68cb2e7d4d9979
andrea <andrea.nuara02@gmail.com>
2024-07-23 00:27:28 +0200

    Aggiunta funzionante lato backend e frontend

--- frontend/lib/UI/pages/shop/carrello.dart
+++ frontend/lib/UI/pages/shop/carrello.dart
@@ -30,6 +30,19 @@ class _CarrelloState extends State<Carrello> {
     }
   }
 
+  Future<void> _removeFromCart(ProdottoCarrelloDTO prodottoCarrelloDTO) async {
+    try {
+      await _cartService.removeProdotto(prodottoCarrelloDTO);
+      setState(() {
+        _cartFuture = _cartService.getCart(); // Refresh the cart
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Errore nella rimozione: $e')),
+      );
+    }
+  }
+
   Future<void> _checkout(Cart c) async {
     try {
       final ordine = await _cartService.checkout(c);
@@ -44,10 +57,18 @@ class _CarrelloState extends State<Carrello> {
     }
   }
 
+  double _calculateTotalPrice(Cart cart) {
+    return cart.prodotti.fold(
+      0.0,
+          (total, prodottoCarrello) =>
+      total + (prodottoCarrello.prodotto.prezzo * prodottoCarrello.quantita),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MyAppBar(onBackFromSuccessivePage: null,),
+      appBar: const MyAppBar(onBackFromSuccessivePage: null),
       body: Column(
         children: [
           Expanded(
@@ -62,72 +83,97 @@ class _CarrelloState extends State<Carrello> {
                   return const Center(child: Text('Il carrello è vuoto.'));
                 } else {
                   final cart = snapshot.data!;
-                  carrello=cart;
-                  return ListView.builder(
-                    itemCount: cart.prodotti.length,
-                    itemBuilder: (context, index) {
-                      final prodottoCarrello = cart.prodotti[index];
-                      final prodotto = prodottoCarrello.prodotto;
-                      return Card(
-                        margin: const EdgeInsets.all(10.0),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Image.asset(prodotto.immagine, width: 220, height: 220),
-                              const SizedBox(width: 20),
-                              Expanded(
-                                child: Column(
+                  carrello = cart;
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: cart.prodotti.length,
+                          itemBuilder: (context, index) {
+                            final prodottoCarrello = cart.prodotti[index];
+                            final prodotto = prodottoCarrello.prodotto;
+                            return Card(
+                              margin: const EdgeInsets.all(10.0),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text(
-                                      prodotto.nome,
-                                      style: const TextStyle(
-                                        fontSize: 20.0,
-                                        fontWeight: FontWeight.bold,
+                                    Image.asset(prodotto.immagine, width: 220, height: 220),
+                                    const SizedBox(width: 20),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            prodotto.nome,
+                                            style: const TextStyle(
+                                              fontSize: 20.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text('Quantità: ${prodottoCarrello.quantita}'),
+                                          Text('Prezzo: \$${prodotto.prezzo.toString()}'),
+                                          Text('Totale: \$${(prodotto.prezzo * prodottoCarrello.quantita).toString()}'),
+                                        ],
                                       ),
                                     ),
-                                    Text('Quantità: ${prodottoCarrello.quantita}'),
-                                    Text('Prezzo: \$${prodotto.prezzo.toString()}'),
-                                    Text('Totale: \$${(prodotto.prezzo * prodottoCarrello.quantita).toString()}'),
+                                    IconButton(
+                                      icon: const Icon(Icons.remove_shopping_cart),
+                                      onPressed: () {
+                                        final prodottoCarrelloDTO = ProdottoCarrelloDTO(
+                                          idProdotto: prodotto.id,
+                                          quantita: prodottoCarrello.quantita,
+                                        );
+                                        _removeFromCart(prodottoCarrelloDTO);
+                                      },
+                                    ),
                                   ],
                                 ),
                               ),
-                            ],
+                            );
+                          },
+                        ),
+                      ),
+                      if (_checkoutError != null)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            _checkoutError!,
+                            style: const TextStyle(color: Colors.red),
                           ),
                         ),
-                      );
-                    },
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Totale Ordine: \$${_calculateTotalPrice(cart).toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (carrello.prodotti.isNotEmpty) {
+                              _checkout(carrello);
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            iconColor: Colors.black,
+                          ),
+                          child: const Text('Checkout'),
+                        ),
+                      ),
+                    ],
                   );
                 }
               },
             ),
           ),
-          if (_checkoutError != null)
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                _checkoutError!,
-                style: const TextStyle(color: Colors.red),
-              ),
-            ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if (carrello != null) {
-                  _checkout(carrello);
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                iconColor: Colors.black,
-              ),
-              child: const Text('Checkout'),
-            ),
-          ),
-
         ],
       ),
     );
@@ -142,7 +188,7 @@ class ConfermaOrdinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MyAppBar(onBackFromSuccessivePage: null,),
+      appBar: const MyAppBar(onBackFromSuccessivePage: null),
       body: Center(
         child: Text('Ordine confermato! ID ordine: ${ordine.id}'),
       ),
